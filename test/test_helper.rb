fe720dcb06bcc5fe1aa29aca66e2ffc505429277
Stefano <gazzottistefano@gmail.com>
2024-01-23 14:46:38 -0300

    complete fixtures and functions to helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,8 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  def encode_token(payload)
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+  end
 end
