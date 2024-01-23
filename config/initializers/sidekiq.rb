e16ac210f2a3ca19ea6ec6b901232224aaaa1952
Stefano <gazzottistefano@gmail.com>
2024-01-23 15:46:55 -0300

    config sidekiq cron

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,13 @@
+schedule_file = "config/schedule.yml"
+
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://localhost:6379/0' }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://localhost:6379/0' }
+end
+
+if File.exist?(schedule_file) && Sidekiq.server?
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  require 'sidekiq/cron/web'
+  mount Sidekiq::Web => '/sidekiq'
+
   namespace :api do
     resources :products
     post '/login', to: 'auth#authenticate'
