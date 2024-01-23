9a3947da9277a1ce926006bad603304766268e1b
Stefano <gazzottistefano@gmail.com>
2024-01-23 15:49:17 -0300

    delete sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  require 'sidekiq/web'
-  require 'sidekiq/cron/web'
-  mount Sidekiq::Web => '/sidekiq'
-
   namespace :api do
     resources :products
     post '/login', to: 'auth#authenticate'
